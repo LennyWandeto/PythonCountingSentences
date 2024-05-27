070d79e05981fea2e0a5a6b7aa3c44e1cd774795
Lenny Wandeto <lenny.wandeto@student.moringaschool.com>
2024-05-27 10:44:58 +0300

    completion

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,37 @@
 #!/usr/bin/env python3
 
+import re
+
 class MyString:
+  def __init__(self, value = ''):
+    self.value = value
+  
+  @property
+  def value(self):
+    return self._value
+
+  @value.setter
+  def value(self, value):
+    if type(value) == str:
+      self._value = value
+    else:
+      print("The value must be a string.")
+    
+  def is_sentence(self):
+    return self._value.endswith('.')
+  
+  def is_question(self):
+    return self._value.endswith('?')
+  
+  def is_exclamation(self):
+    return self._value.endswith('!')
+  
+  def count_sentences(self):
+    value = self.value
+    for punc in ['!', '?']:
+      value = value.replace(punc, '.')
+    
+    sentence = [s for s in value.split('.') if s]
+  
+    return len(sentence)
   pass
